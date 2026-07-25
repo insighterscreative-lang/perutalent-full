8406537c9213299b74a6c92535fa385ee5d48030
Renato Alcalde <renatoalcalde285@gmail.com>
2026-07-25 16:36:07 -0500

    Fix Configuracion S3 HTTP

--- backend/src/main/java/com/INSIGHTERS_PERU/Up/Work/Perusalen/config/AwsS3Config.java
+++ backend/src/main/java/com/INSIGHTERS_PERU/Up/Work/Perusalen/config/AwsS3Config.java
@@ -3,6 +3,7 @@ package com.INSIGHTERS_PERU.Up.Work.Perusalen.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,6 +17,7 @@ public class AwsS3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
+                .httpClientBuilder(UrlConnectionHttpClient.builder())
                 .build();
     }
 }
